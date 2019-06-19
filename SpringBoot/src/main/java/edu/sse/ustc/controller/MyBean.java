2371bc0c6fb6024e05d6b276a903e838e7760ceb
LeiCode <imarklei90@126.com>
2019-06-19 09:57:45 +0800

    Add files via upload

---
+++ SpringBoot/src/main/java/edu/sse/ustc/controller/MyBean.java
@@ -0,0 +1,42 @@
+package edu.sse.ustc.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Bean中注入值@Value
+ */
+@Component
+public class MyBean {
+
+    @Value("${application.name}")
+    private String name;
+
+    @Value("${application.description}")
+    private String description;
+
+    public MyBean(){
+
+    }
+
+    public MyBean(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
