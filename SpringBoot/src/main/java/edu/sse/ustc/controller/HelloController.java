033e2a204f7698b8e54f123e3e831f43e73b8f6e
LeiCode <imarklei90@126.com>
2019-06-19 09:59:00 +0800

    Add files via upload

--- SpringBoot/src/main/java/edu/sse/ustc/controller/HelloController.java
+++ SpringBoot/src/main/java/edu/sse/ustc/controller/HelloController.java
@@ -1,5 +1,6 @@
 package edu.sse.ustc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,15 +17,38 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration(exclude = RedisAutoConfiguration.class) // 取消redis的自动配置
 public class HelloController {
 
+	/**
+	 * 简单返回值
+	 * @return
+	 */
 	@RequestMapping("/hello")
 	public String hello(){
 		return "Hello Spring";
 	}
 
+	/**
+	 * URL带参数返回
+	 * @param value
+	 * @return
+	 */
 	@RequestMapping("/sayHello/{value}")
 	@ResponseBody
 	public String sayHello(@PathVariable String value){
 		return "Hello " + value;
 	}
 
+	@Autowired
+	private MyBean myBean;
+
+	/**
+	 * 获取Application.properties中的属性值
+	 * @return
+	 */
+	@RequestMapping("/testMyBean")
+	@ResponseBody
+	public String testMyBean(){
+		String name = myBean.getName();
+		String description = myBean.getDescription();
+		return "Application is : " + name + ", Description: " + description;
+	}
 }
